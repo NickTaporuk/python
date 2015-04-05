076a8aeacd3209df2fe8a31b18fd175e97ae14c8
NickTaporuk <nictaporuk@yandex.ru>
2015-04-05 22:51:33 +0300

    fix prolem to unicode in file save

--- parser.py
+++ parser.py
@@ -3,6 +3,10 @@
 __author__ = 'nictaporuk@yandex.ru'
 __copyright__ = 'Copyright 2015 NickTaporuk'
 __version__ = '0.0.1'
+# Устанавливаем стандартную внешнюю кодировку = utf8
+# import sys
+# reload(sys)
+# sys.setdefaultencoding('utf8')
 from urllib import urlopen
 from BeautifulSoup import BeautifulSoup, SoupStrainer
 import sys, time, os
@@ -10,6 +14,9 @@ from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import mapper
 from sqlalchemy.orm import sessionmaker
+import simplejson as json
+from pprint import pprint
+
 URL = 'http://www.lun.ua/%D0%BF%D1%80%D0%BE%D0%B4%D0%B0%D0%B6%D0%B0-%D0%BA%D0%B2%D0%B0%D1%80%D1%82%D0%B8%D1%80-%D0%BA%D0%B8%D0%B5%D0%B2?page=2&roomCount=1&roomCount=2'
 MAX_PAGE = 100
 """
@@ -28,26 +35,28 @@ def get_html(url):
 
 """
 def get_lun_article(url =URL):
-    obj_right = []
-    obj_left = []
+    obj_right   = []
+    obj_left    = []
+    area        = []
     c = 0
     link = SoupStrainer('div')
     for i in BeautifulSoup(get_html(url), parseOnlyThese=link).findAll('div', { "class" : "obj" }):
         # return i['class']
-
+        # s2 = u''.join(unichr(ord(c)) for c in s)
         for ii in i.findAll('div', {'class': "obj-right"}):
             v = ii.span
             obj_right.append([v.string[v.string.find('$')], v.string.replace('$','')])# obj_right
 
         for ii in i.findAll('div', {'class': "obj-left"}):
-            v = ii.h3
-            return v
-            # obj_left.append([v.string[v.string.find('$')], v.string.replace('$','')])# obj_right
-
-            #  obj = {ii.span.string.find('$')} # currency
-    # return soup.findAll('div', { "class" : "obj" })
-    # return len(obj_right)
-    # return obj_right
+            obj_left.append(ii.h3.a.string.split(','))
+            for iii in ii.findAll('div', {'class': "obj-locality"}):
+                if iii.string != None:
+                    area.append(iii.string)
+                else:
+                    area.append(iii.contents[0])
+    # return len(area)
+    all = zip(obj_right, obj_left, area)
+    return all[0]
 """
 lun.ua get pagination
 """
@@ -89,7 +98,13 @@ def main():
     # print get_url_lun()
     # print get_pagination(1)
     # print table.columns
-    set_data_to_file('article.txt',get_lun_article())
+    # a = ['ущшпрцуп','ущамоуцша']
+    # print a[0]
+    # print u'ущшпрцуп'
+    t = get_lun_article()
+    print len(t[0][0])
+    # set_data_to_file('article.txt',get_lun_article())
+    set_data_to_file('article.txt', t[0][1])
     # pass
 
 if __name__ == '__main__':
