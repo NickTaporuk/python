b25b2ec07b4839db04a24947f127197dede06157
Nickolay Kuropatkin <nictaporuk@yandex.ru>
2015-08-04 18:25:42 +0300

    fix

--- ssh/sonarSshClient.py
+++ ssh/sonarSshClient.py
@@ -5,9 +5,9 @@ __copyright__ = 'Copyright 2015 NickTaporuk'
 __version__ = '0.0.1'
 
 BASH_SONAR_RUN_PATH = '/home'
-PASSW = '01041987'
-USER  = 'nkuropatkin'
-HOST  = 'sonar.itftc.com'
+PASSW = '*'
+USER  = '*'
+HOST  = '*'
 
 import paramiko
 ssh=paramiko.SSHClient()
@@ -24,12 +24,6 @@ ssh.connect(HOST,username=USER,password=PASSW)
 '''
     Run crush server ;)
 '''
-#
-# ssh.exec_command("cd /var/nabludai | ")
-#
-# ssh.exec_command("git pull origin master")
-#
-# stdin, stdout, stderr = ssh.exec_command("sudo /etc/init.d/sonar-runner")
 stdin, stdout, stderr = ssh.exec_command("cd /var/www/nabludai && git pull origin master && bash sudo /etc/init.d/sonar-runner")
 # stdin.write("01041987\n")
 # stdin.flush()
