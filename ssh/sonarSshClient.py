ea1ba30bf240611d62f48d01fc43dba55f4ec7b6
Nickolay Kuropatkin <nictaporuk@yandex.ru>
2015-08-07 17:36:48 +0300

    fix

--- ssh/sonarSshClient.py
+++ ssh/sonarSshClient.py
@@ -5,9 +5,9 @@ __copyright__ = 'Copyright 2015 NickTaporuk'
 __version__ = '0.0.1'
 
 BASH_SONAR_RUN_PATH = '/home'
-PASSW = '*'
-USER  = '*'
-HOST  = '*'
+PASSW = '01041987'
+USER  = 'nkuropatkin'
+HOST  = 'sonar.itftc.com'
 
 import paramiko
 ssh=paramiko.SSHClient()
@@ -24,13 +24,31 @@ ssh.connect(HOST,username=USER,password=PASSW)
 '''
     Run crush server ;)
 '''
-stdin, stdout, stderr = ssh.exec_command("cd /var/www/nabludai && git pull origin master && bash sudo /etc/init.d/sonar-runner")
-# stdin.write("01041987\n")
-# stdin.flush()
+
+ssh.exec_command("cd /var/www/nabludai ")
+
+ssh.exec_command("git pull origin master")
+channel = ssh.get_transport().open_session()
+channel.get_pty()
+channel.settimeout(5)
+channel.exec_command('sudo -s')
+channel.send(PASSW+'\n')
+print channel.recv(1024)
+
+stdin, stdout, stderr = ssh.exec_command("sudo -s")
+stdin.write("01041987\n")
+stdin.flush()
+
+stdin, stdout, stderr = ssh.exec_command("whoami")
+# stdin, stdout, stderr = ssh.exec_command("sudo /etc/init.d/sonar-runner")
+# stdin, stdout, stderr = ssh.exec_command("cd /var/www/nabludai && git pull origin master && sudo /etc/init.d/sonar-runner")
+
 error = stderr.read().splitlines()
 out   = stdout.read().splitlines()
 # inn   = stdin.read().splitlines()
 print error,out
+
+ssh.close()
 # stdin, stdout, stderr = ssh.exec_command("sudo -S reboot")
 # stdin.write("< пароль пользователя>\n")
 # stdin.flush()
